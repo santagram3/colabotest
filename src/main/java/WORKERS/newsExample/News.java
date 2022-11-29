2b38b0e5f724ef9df815ef0420309fb70aa2d9f1
santagram3 <soslimso@naver.com>
2022-11-29 23:42:18 +0900

    Team1

---
+++ src/main/java/WORKERS/newsExample/News.java
@@ -0,0 +1,41 @@
+package WORKERS.newsExample;
+
+public class News {
+	private int aid;
+	private String title;
+	private String img;
+	private String regDate;
+	private String content;
+	public int getAid() {
+		return aid;
+	}
+	public void setAid(int aid) {
+		this.aid = aid;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getImg() {
+		return img;
+	}
+	public void setImg(String img) {
+		this.img = img;
+	}
+	public String getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(String regDate) {
+		this.regDate = regDate;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
+}
